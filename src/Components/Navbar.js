48a75c7191bd37e6281644f0fa686f8087b62b0d
oindil <oindilgolder303@gmail.com>
2023-06-13 20:25:21 +0530

    started today's task

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,32 +6,17 @@ export class Navbar extends Component {
 
   render() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary font-monospace">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#"><strong style={{fontFamily:"abril fatface"}}>The Current Times</strong></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
+
             <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <input className="form-control me-2" type="search" placeholder="Search for news" aria-label="Search"/>
+              <button className="btn btn-sm btn-success" type="submit">Search</button>
             </form>
           </div>
         </div>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 export default class extends Component {
+  constructor(){
+    super()
+    // console.log("hello I am a constructor")
+  }
   render() {
     let { title, description,imageUrl } = this.props;
     return (
-      <div>
+      <div className="my-4">
         <div className="card" style={{ width: "18rem" }}>
         <img src={imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
@@ -12,7 +16,7 @@ export default class extends Component {
             <p className="card-text">
               {description}
             </p>
-            <a href="#" className="btn btn-primary">
+            <a href="#" className="btn btn-sm btn-success">
               Read More
             </a>
           </div>
